69eb46a4f897cfd8a966a6e3abba467d6a883b16
jhonatan050 <jhonatanchango9@gmail.com>
2026-04-12 22:37:18 -0500

    Guardar Restar.java

---
+++ src/Restar.java
@@ -0,0 +1,9 @@
+package src;
+public class Restar{
+    public static void main(String[] args) {
+        int a=10;
+        int b=5;
+        int resultado = a-b;
+        System.out.println("El resultado de la resta es: "+resultado);
+    }
+}
